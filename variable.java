7d8ad39fddcef1c48f4f047f95fc94caf43cd107
Anany-Pandey <110780191+Anany-Pandey@users.noreply.github.com>
2024-04-30 13:31:39 +0530

    variable.java

---
+++ variable.java
@@ -0,0 +1,11 @@
+package mat;
+public class variable{
+public double value;
+public Double derivative=0.0;
+public variable(double a){
+this.value=a;
+}
+public String toString(){
+return "value ="+Double.toString(value)+"\tderivative ="+derivative.toString();
+}  
+}
